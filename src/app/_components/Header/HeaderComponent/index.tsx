9a30e71c2157f60d05d321a178ed04763c5b9ce0
Abu Jayed <aj.webdevlopment@gmail.com>
2023-12-18 09:52:19 +0600

    button and eslint customized.

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
@@ -5,11 +6,18 @@ import { Gutter } from '../../Gutter'
 import classes from './index.module.scss'
 import Image from 'next/image'
 import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
   return (
     <>
-      <nav className={[classes.header]}>
+      <nav
+        className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+          .filter(Boolean)
+          .join(' ')}
+      >
         <Gutter className={classes.wrap}>
           <Link href={'/'}>
             <Image width={170} height={50} src="/logo-black.svg" alt="logo" />
